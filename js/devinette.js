3ca05aaf5440dd0b48c55afa33f23f5b045fd861
dhiasouidi <dhiasouidii@gmail.com>
2019-03-27 20:02:53 +0100

    test du script1

--- js/devinette.js
+++ js/devinette.js
@@ -1,21 +1,22 @@
 console.log("Bienvenue dans ce jeu de devinette !");
- 
-var ran = Math.floor(Math.random() * 100) + 1;
- 
+
 var input = Number(prompt("Entrez un nombre :"));
-while(input !== ran)
+var ran = Math.floor(Math.random() * 100) + 1;
+var n=0;
+ 
+while((input !== ran)&&(n<5))
 {
   if (input > ran)
-  {
-    console.log(input + " est trop grand");
-    input = Number(prompt("Retente ta chance !"))
-  }
- 
+  {console.log(input + " est trop grand");}
   else if (input < ran)
-  {
-    console.log(input + " est trop petit");
-    input = Number(prompt("Retente ta chance !"))
-  }
+  {console.log(input + " est trop petit");}
+  n++;
+  input = Number(prompt("Retente ta chance !"))
+
+
 }
 if(input == ran)
-  console.log("Bravo !! La solution est : " + ran);
+  console.log("Bravo !! ");
+else
+  console.log("Vous avez perdu ! La solution est : " + ran)
+var n1=prompt("veuillez vous rejouer ? 0/1")
